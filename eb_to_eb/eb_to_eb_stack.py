03a4d68b2843e0ce8fc75050bca6933382e1e6ce
Pubudu Jayawardana <pubudu.jayawardana@postnl.nl>
2023-07-14 22:49:59 +0200

    Initial project

--- eb_to_eb/eb_to_eb_stack.py
+++ eb_to_eb/eb_to_eb_stack.py
@@ -1,19 +1,72 @@
 from aws_cdk import (
-    # Duration,
+    CfnOutput,
     Stack,
-    # aws_sqs as sqs,
+    aws_events_targets as events_target,
+    aws_events as events,
+    aws_logs as logs,
+    aws_sqs as sqs,
 )
 from constructs import Construct
 
-class EbToEbStack(Stack):
 
+class EbToEbStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Add event buses
+        events_bus_a = events.EventBus(self, "EventsBusA")
+        events_bus_b = events.EventBus(self, "EventsBusB")
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "EbToEbQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Dead Letter Queues
+        dlq_a = sqs.Queue(self, "DLQForBusA")
+        dlq_b = sqs.Queue(self, "DLQForBusB")
+
+        # Add catch all log groups
+        catch_all_log_group_a = logs.LogGroup(
+            self, "CatchAllLogGroupA", retention=logs.RetentionDays.ONE_DAY
+        )
+        catch_all_log_group_b = logs.LogGroup(
+            self, "CatchAllLogGroupB", retention=logs.RetentionDays.ONE_DAY
+        )
+
+        # Add event bus rule to match all the events
+        event_bus_rule_a = events.Rule(
+            self,
+            "eventRuleForLogGroupA",
+            event_bus=events_bus_a,
+            event_pattern=events.EventPattern(version=["0"]),
+        )
+
+        # Add Cloudwatch as one target for the rule
+        event_bus_rule_a.add_target(
+            events_target.CloudWatchLogGroup(catch_all_log_group_a)
+        )
+        # Add the other event bus as another target for the rule
+        event_bus_rule_a.add_target(
+            events_target.EventBus(event_bus=events_bus_b, dead_letter_queue=dlq_a)
+        )
+
+        # Add event bus rule to match all the events
+        event_bus_rule_b = events.Rule(
+            self,
+            "eventRuleForLogGroupB",
+            event_bus=events_bus_b,
+            event_pattern=events.EventPattern(version=["0"]),
+        )
+
+        # Add Cloudwatch as one target for the rule
+        event_bus_rule_b.add_target(
+            events_target.CloudWatchLogGroup(catch_all_log_group_b)
+        )
+        # Add the other event bus as another target for the rule
+        event_bus_rule_b.add_target(
+            events_target.EventBus(event_bus=events_bus_a, dead_letter_queue=dlq_b)
+        )
+
+        # Output
+        CfnOutput(self, "EventBusA", value=events_bus_a.event_bus_arn)
+        CfnOutput(self, "EventBusB", value=events_bus_b.event_bus_arn)
+        CfnOutput(self, "SQS Queue A", value=dlq_a.queue_name)
+        CfnOutput(self, "SQS Queue B", value=dlq_b.queue_name)
+        CfnOutput(self, "Log Gruoup A", value=catch_all_log_group_a.log_group_name)
+        CfnOutput(self, "Log Gruoup B", value=catch_all_log_group_b.log_group_name)
